310b15f99635855d34396c31ab9cdf2172dfc8fd
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-02-06 17:01:20 +0200

    Armies with a single soldier each

--- armysim.gd
+++ armysim.gd
@@ -2,51 +2,54 @@ extends Node
 
 const Soldier = preload("Soldier.tscn")
 
-var soldierA
-var soldierB
+var armyA
+var armyB 
 
 func create_soldiers_for_test(): 
-	soldierA = Soldier.instance()
+	armyA = []
+	armyB = []
+	
+	var soldierA = Soldier.instance()
 	soldierA.hp = 5
 	soldierA.display_name = "Soldier A"
+	armyA.append(soldierA)
 	
-	soldierB = Soldier.instance()
+	var soldierB = Soldier.instance()
 	soldierB.hp = 3
 	soldierB.display_name = "Soldier B"
+	armyB.append(soldierB)
 
 func _ready():
 	create_soldiers_for_test()
-	var attacker = soldierA
-	var defender = soldierB
+	var attack_army = armyA
+	var defend_army = armyB
 	
 	while (true): 
-		attacker_attacks_defender(attacker, defender)
-		if defender.hp <= 0: 
-			print(defender.display_name + " IS DEAD")
+		attacking_army_attacks_defending_army(attack_army, defend_army)
+		if defend_army.size() == 0: 
 			break
 		
-		var temp = defender
-		defender = attacker
-		attacker = temp
-		
-
+		var temp = defend_army
+		defend_army = attack_army
+		attack_army = temp
 	
 	print("")
 	print("Attack done")
-	if soldierA.hp <= 0:
-		print(soldierB.display_name + " WINS")
-	elif soldierB.hp <= 0:
-		print(soldierA.display_name + " IS DEAD")
-	else: 
-		print("No-one wins")
-
-
 
 func _process(delta):
 	pass
+
+func attacking_army_attacks_defending_army(attack_army, defend_army): 
+	var attacker = attack_army[0]
+	var defender = defend_army[0]
+	attacker_attacks_defender(attacker, defender)
+	if defender.hp <= 0:
+		print(defender.display_name + " IS DEAD")
+		defend_army.pop_front()
 
 func attacker_attacks_defender(attacker, defender): 
 	print(attacker.display_name + " attacks " + defender.display_name)
 	defender.hp -= 1
 	print(defender.display_name + " HP is now: " + str(defender.hp))
+		
 	
