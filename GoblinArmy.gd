ae097df12e8c587ec355d016ffe8ce761208d899
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-02-06 14:03:00 +0200

    Remove everything

--- GoblinArmy.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-export var speed = 20
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	position.x =  position.x + delta * speed
