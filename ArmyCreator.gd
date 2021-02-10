ffdb9f846e88ab24050cd57c8023394532405c7d
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-02-10 22:03:25 +0200

    Army height variable.

--- ArmyCreator.gd
+++ ArmyCreator.gd
@@ -3,6 +3,8 @@ extends Node
 const Soldier = preload("Soldier.tscn")
 const Army = preload("Army.tscn")
 
+const army_height = 92
+
 func create_soldiers_for_test(): 
 	var armyA = Army.instance()
 	var armyB = Army.instance()
@@ -13,19 +15,22 @@ func create_soldiers_for_test():
 	soldier = Soldier.instance()
 	soldier.hp = 1
 	soldier.display_name = "A1"
-	soldier.position = Vector2(32,100)
+	soldier.position = Vector2(32,army_height)
+	soldier.set_sprite_idle()
 	armyA.add_soldier(soldier)
 		
 	soldier = Soldier.instance()
 	soldier.hp = 2
 	soldier.display_name = "A2"
-	soldier.position = Vector2(64,100)
+	soldier.position = Vector2(64,army_height)
+	soldier.set_sprite_idle()
 	armyA.add_soldier(soldier)
 	
 	soldier = Soldier.instance()
 	soldier.hp = 3
 	soldier.display_name = "A3"
-	soldier.position = Vector2(96,100)
+	soldier.position = Vector2(96,army_height)
+	soldier.set_sprite_attack()
 	armyA.add_soldier(soldier)
 	
 	armyB.display_name = "Army B"
@@ -33,14 +38,16 @@ func create_soldiers_for_test():
 	soldier = Soldier.instance()
 	soldier.hp = 3
 	soldier.display_name = "B1"
-	soldier.position = Vector2(160,100)
+	soldier.position = Vector2(160,army_height)
+	soldier.set_sprite_defend()
 	soldier.face_left()
 	armyB.add_soldier(soldier)
 	
 	soldier = Soldier.instance()
 	soldier.hp = 5
 	soldier.display_name = "B2"
-	soldier.position = Vector2(192,100)
+	soldier.position = Vector2(192,army_height)
+	soldier.set_sprite_idle()
 	soldier.face_left()
 	armyB.add_soldier(soldier)
 	
