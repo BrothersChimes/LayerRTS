4b8e05771b69c96b129e4c42d7176afc8fbfddac
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-02-10 21:54:50 +0200

    Soldier sprites

--- ArmyCreator.gd
+++ ArmyCreator.gd
@@ -13,16 +13,19 @@ func create_soldiers_for_test():
 	soldier = Soldier.instance()
 	soldier.hp = 1
 	soldier.display_name = "A1"
+	soldier.position = Vector2(32,100)
 	armyA.add_soldier(soldier)
 		
 	soldier = Soldier.instance()
 	soldier.hp = 2
 	soldier.display_name = "A2"
+	soldier.position = Vector2(64,100)
 	armyA.add_soldier(soldier)
 	
 	soldier = Soldier.instance()
 	soldier.hp = 3
 	soldier.display_name = "A3"
+	soldier.position = Vector2(96,100)
 	armyA.add_soldier(soldier)
 	
 	armyB.display_name = "Army B"
@@ -30,11 +33,15 @@ func create_soldiers_for_test():
 	soldier = Soldier.instance()
 	soldier.hp = 3
 	soldier.display_name = "B1"
+	soldier.position = Vector2(160,100)
+	soldier.face_left()
 	armyB.add_soldier(soldier)
 	
 	soldier = Soldier.instance()
 	soldier.hp = 5
 	soldier.display_name = "B2"
+	soldier.position = Vector2(192,100)
+	soldier.face_left()
 	armyB.add_soldier(soldier)
 	
 	return [armyA, armyB]
