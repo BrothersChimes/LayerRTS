62646760de5337cc11bbc67cbc5f7f8013783bbf
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-02-25 19:41:48 +0200

    Big army demo

--- ArmyCombatSim.gd
+++ ArmyCombatSim.gd
@@ -6,14 +6,15 @@ var armyB
 var attack_army
 var defend_army
 
-const phase_attack_allowed_time = 0.2
-const phase_defend_allowed_time = 0.2
-const phase_death_allowed_time = 0.4
-const phase_cycle_allowed_time = 0.4
+const phase_attack_allowed_time = 0.3
+const phase_defend_allowed_time = 0.3
+const phase_death_allowed_time = 0.3
+const phase_cycle_allowed_time_if_alive = 0.3
+const phase_cycle_allowed_time_if_dead = 0.6
 
 var time_to_next_phase = 0
 
-enum Phase {ATTACK, DEATH_CHECK, DEFEND, DEATH, CYCLE}
+enum Phase {ATTACK, DEATH_CHECK, DEFEND, DEATH, CYCLE_LIVE, CYCLE_DEAD}
 var phase = Phase.ATTACK
 
 enum State {OUT_OF_COMBAT, COMBAT, END_COMBAT}
@@ -56,14 +57,19 @@ func perform_combat_state_action(delta):
 		elif phase == Phase.DEFEND: 
 			defend_phase()
 			time_to_next_phase = phase_defend_allowed_time
-			phase = Phase.CYCLE
+			phase = Phase.CYCLE_LIVE
 		elif phase == Phase.DEATH: 
 			death_phase()
 			time_to_next_phase = phase_death_allowed_time
-			phase = Phase.CYCLE
-		elif phase == Phase.CYCLE: 
+			phase = Phase.CYCLE_DEAD
+		elif phase == Phase.CYCLE_LIVE: 
 			cycle_phase()
-			time_to_next_phase = phase_cycle_allowed_time
+			time_to_next_phase = phase_cycle_allowed_time_if_alive
+			switch_attacker()
+			phase = Phase.ATTACK
+		elif phase == Phase.CYCLE_DEAD: 
+			cycle_phase()
+			time_to_next_phase = phase_cycle_allowed_time_if_dead
 			switch_attacker()
 			phase = Phase.ATTACK
 	else: 
--- ArmyCreator.gd
+++ ArmyCreator.gd
@@ -6,12 +6,15 @@ const Army = preload("Army.tscn")
 const army_height = 96
 const distance_between_soldiers = 32
 
-const armyAxPos = 96
-const armyBxPos = 160
+const armyAxPos = 192
+const armyBxPos = armyAxPos + distance_between_soldiers
+
+var armyA
+var armyB
 
 func create_soldiers_for_test(): 
-	var armyA = Army.instance()
-	var armyB = Army.instance()
+	armyA = Army.instance()
+	armyB = Army.instance()
 	var soldier
 	
 	armyA.display_name = "Army A"
@@ -19,51 +22,42 @@ func create_soldiers_for_test():
 	armyA.set_location(Vector2(armyAxPos, army_height))
 	armyA.is_facing_right = true
 		
-	soldier = Soldier.instance()
-	soldier.hp = 1
-	soldier.display_name = "A1"
-	soldier.position = Vector2(armyAxPos,army_height)
-	soldier.expected_x_position = soldier.position.x
-	soldier.set_sprite_idle()
-	armyA.add_soldier(soldier)
-		
-	soldier = Soldier.instance()
-	soldier.hp = 2
-	soldier.display_name = "A2"
-	soldier.position = Vector2( armyAxPos-distance_between_soldiers, army_height)
-	soldier.expected_x_position = soldier.position.x
-	soldier.set_sprite_idle()
-	armyA.add_soldier(soldier)
-	
-	soldier = Soldier.instance()
-	soldier.hp = 3
-	soldier.display_name = "A3"
-	soldier.position = Vector2(armyAxPos-distance_between_soldiers*2, army_height)
-	soldier.expected_x_position = soldier.position.x
-	soldier.set_sprite_idle()
-	armyA.add_soldier(soldier)
+	create_soldier_A(1,1)
+	create_soldier_A(2,2)
+	create_soldier_A(3,3) 
+	create_soldier_A(4,4) 
+	create_soldier_A(5,5) 
+	create_soldier_A(6,6)
 	
 	armyB.display_name = "Army B"
 	armyB.distance_between_soldiers = distance_between_soldiers
 	armyB.set_location(Vector2(armyBxPos, army_height))
 	armyB.is_facing_right = false
 	
-	soldier = Soldier.instance()
-	soldier.hp = 3
-	soldier.display_name = "B1"
-	soldier.position = Vector2(armyBxPos,army_height)
-	soldier.expected_x_position = soldier.position.x
-	soldier.set_sprite_idle()
-	soldier.face_left()
-	armyB.add_soldier(soldier)
-	
-	soldier = Soldier.instance()
-	soldier.hp = 5
-	soldier.display_name = "B2"
-	soldier.position = Vector2(armyBxPos + distance_between_soldiers,army_height)
-	soldier.expected_x_position = soldier.position.x
-	soldier.set_sprite_idle()
-	soldier.face_left()
-	armyB.add_soldier(soldier)
+	create_soldier_B(1,6)
+	create_soldier_B(2,5)
+	create_soldier_B(3,4)
+	create_soldier_B(4,3)
+	create_soldier_B(5,2)
+	create_soldier_B(6,1)
 	
 	return [armyA, armyB]
+
+func create_soldier_A(number, hp): 
+	var soldier = Soldier.instance()
+	soldier.hp = hp
+	soldier.display_name = "A" + str(number)
+	soldier.position = Vector2(armyAxPos-distance_between_soldiers*(number-1),army_height)
+	soldier.expected_x_position = soldier.position.x
+	soldier.set_sprite_idle()
+	armyA.add_soldier(soldier)
+	
+func create_soldier_B(number, hp): 
+	var soldier = Soldier.instance()
+	soldier.hp = hp
+	soldier.display_name = "B" + str(number)
+	soldier.position = Vector2(armyBxPos+distance_between_soldiers*(number-1),army_height)
+	soldier.expected_x_position = soldier.position.x
+	soldier.set_sprite_idle()
+	soldier.face_left()
+	armyB.add_soldier(soldier)
