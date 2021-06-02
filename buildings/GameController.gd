5d1b3838214c00b7a9cb8f740b6484a4f0956baa
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-06-02 12:49:24 +0200

    Closest barracks and closest nonbusy builder.

--- buildings/GameController.gd
+++ buildings/GameController.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const MAX_INT = 9223372036854775807
+
 var barrackses_to_builders_dict = {}
 var unbuilt_barrackses = []
 
@@ -13,6 +15,9 @@ func _on_UnitsController_register_units():
 
 func _on_BuildingsController_barracks_placed(barracks_builder):
 	# Send a builder to start building
+	find_closest_nonbusy_builder(barracks_builder)
+
+func find_first_nonbusy_builder(barracks_builder): 
 	var builders = $UnitsController.builders
 	for builder in builders: 
 		if not builder.is_busy(): 
@@ -20,6 +25,25 @@ func _on_BuildingsController_barracks_placed(barracks_builder):
 			barrackses_to_builders_dict[barracks_builder] = builder
 			return
 	unbuilt_barrackses.append(barracks_builder)
+	
+func find_closest_nonbusy_builder(barracks_builder): 
+	var builders = $UnitsController.builders
+	var closest_builder = null
+	var closest_distance = MAX_INT
+	
+	for builder in builders: 
+		if not builder.is_busy(): 
+			var dist = abs(builder.position.x - barracks_builder.position.x)
+			if dist < closest_distance: 
+				closest_distance = dist
+				closest_builder = builder
+
+	if closest_builder == null: 
+		unbuilt_barrackses.append(barracks_builder)
+		return
+	
+	closest_builder.set_target_building(barracks_builder)
+	barrackses_to_builders_dict[barracks_builder] = closest_builder
 
 func _on_Builder_start_building(builder): 
 	builder.get_target_building().start_building()
@@ -34,7 +58,24 @@ func _on_BuildingsController_barracks_done(barracks_builder):
 		builder.idle()
 		return 
 
+	build_closest_barracks(builder)
+
+func build_closest_barracks(builder): 
+	var closest_barracks
+	var closest_distance = MAX_INT
+	 
+	for unbuilt_barracks in unbuilt_barrackses:
+		if abs(unbuilt_barracks.position.x - builder.position.x) < closest_distance:
+			closest_barracks = unbuilt_barracks
+			closest_distance = abs(unbuilt_barracks.position.x - builder.position.x)
+	
+	print("I am at: " + str(builder.position.x) +  " and the closest barracks is " + str(closest_barracks.position.x))
+	unbuilt_barrackses.remove(unbuilt_barrackses.find(closest_barracks))
+	builder.set_target_building(closest_barracks)
+	barrackses_to_builders_dict[closest_barracks] = builder
+		
+
+func build_first_placed_barracks(builder): 
 	var unbuilt_barracks = unbuilt_barrackses.pop_front()
 	builder.set_target_building(unbuilt_barracks)
 	barrackses_to_builders_dict[unbuilt_barracks] = builder
-		
