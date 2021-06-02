245448e5e7aacb88a29082103b1b900814d50b19
Mark Chimes <mark-chimes@users.noreply.github.com>
2021-06-02 10:39:04 +0200

    Simple check for building collision.

---
+++ PlacementController.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+const ground_level = 162
+const barracks_width = 128
+
+var Barracks = preload("buildings/Barracks.tscn")
+var barracks_placer
+
+var placed_barrackses = []
+
+const arbitrary_allowed_pos_left = -430
+const arbitrary_allowed_pos_right = 1350
+
+func _ready():
+	barracks_placer = $BarracksPlacer
+
+func _process(delta):
+	if get_is_allowed():
+		barracks_placer.set_allowed()
+	else: 
+		barracks_placer.set_not_allowed()
+
+func _on_BarracksPlacer_place_barracks(x_pos):
+	var barracks = Barracks.instance()
+	barracks.position = Vector2(x_pos, ground_level)
+	# barracks.z_index = 2
+	placed_barrackses.append(barracks)
+	add_child(barracks)
+
+func get_is_allowed(): 
+	var position_x = barracks_placer.get_position_x()
+	if position_x > arbitrary_allowed_pos_left and position_x < arbitrary_allowed_pos_right: 
+		if not get_is_barracks_present(position_x):
+			return true
+	return false
+
+#TODO this will get inefficient eventually, especially since it is called every _process
+func get_is_barracks_present(pos): 
+	for barracks in placed_barrackses:
+		if abs(barracks.position.x - pos) < barracks_width:
+			return true
+	return false
--- buildings/BarracksPlacer.gd
+++ buildings/BarracksPlacer.gd
@@ -4,8 +4,6 @@ signal place_barracks(x_pos)
 
 const ground_level = 162
 
-const arbitrary_allowed_pos_left = -430
-const arbitrary_allowed_pos_right = 1350
 
 const snap_size = 32
 var is_allowed = false
@@ -16,16 +14,14 @@ func _ready():
 	
 func _process(delta):
 	position.x = round(get_global_mouse_position().x/snap_size)*snap_size
-	
-	if position.x > arbitrary_allowed_pos_left and position.x < arbitrary_allowed_pos_right: 
-		set_allowed()
-	else: 
-		set_not_allowed()
 
 func _input(event):
 	if (event is InputEventMouseButton and event.is_pressed() and event.button_index == BUTTON_LEFT):
 		if is_allowed:
 			emit_signal("place_barracks", position.x)
+			
+func get_position_x(): 
+	return position.x
 
 func set_allowed(): 
 	is_allowed = true
